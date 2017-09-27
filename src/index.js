f9a00e5d04f18cdea76bd2ddaf523f08e76c4b3c
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-27 19:27:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,75 @@ module.exports = function check(str, bracketsConfig)
   var bool=true;//значение ответа функции
   var i=str.length;
   i=+i;  
-  //alert("V stroke simvolov:  "+(i));// длина строки
+  //alert("V stroke simvolov:  "+i);// длина строки
  
-var s;
+var s,lih=0;
 var bracketsConfig2=[];
 bracketsConfig2=bracketsConfig;
 s=bracketsConfig2.length;
-//alert("Dlina massiva proverki:  "+(s));
-if(i%2==1)
+
+//alert("Dlina massiva proverki:  "+s);
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////
+  
+function poisk(str,bracketsConfig,s,i)//считаем кол-во скобок одного вида
+{ 
+  var f=0;
+  s=s-1;
+  var first=0;
+  var second=0;
+  for(var j=(i-1); j>=0; j--)//пробегаем по строке
+{
+var elem=str.charAt(j);//доступ к элементам строки
+//alert(elem);
+//alert(s);
+//alert(bracketsConfig[s][0]);
+
+    if(elem==bracketsConfig[s][0])
+      {
+        first++; 
+      }
+   
+       else if(elem==bracketsConfig[s][1])
+      {
+        second++; 
+      }
+
+
+}
+
+    if(first!=second)
   {
-    //alert("Srazu otbrosili nehetno, false");
-    bool=false;
-} else{
+   f=1;
+  }
+  return f;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+var f=0;
+for(var u=0;u<s;u++)
+{
+  var h=0;
+f=poisk(str,bracketsConfig,s,i);
+h=h+f;
+}
+
+if(h>0)
+ {bool=false;}
+else{ 
  
   for(var j=0; j<(i/2-1);j++)
   {
   str=del(str, bracketsConfig, s, i,bool);
-  //alert('New str:   '+str); 
+ // alert('New str:   '+str); 
   }
-  
+
+
+
+
 ////////берем строку и удаляем правильные скобки///////////////////////////
 
 function del(str, bracketsConfig, s, i,bool)
@@ -30,7 +80,7 @@ function del(str, bracketsConfig, s, i,bool)
 
   for(var j=i; j>0; j--)//пробегаем по строке
   {
-var a=true;
+
 var elem=str.charAt(j);//доступ к элементам строки
 //alert(elem);
 //alert(bracketsConfig[0][0]);//доступ к элементам подмассивов
@@ -56,26 +106,26 @@ for(var u=0;u<s;u++)
             {
             //alert("NAtknulis na PUSTOTY, false");
             bool=false;
-          }
-          }
 
-                 
+          }
+          }
+             
       }
 
 }
-    
+   
   }
- 
-return newstr;
+ return newstr;
 }
 ////////////////////////////////////////////////////////
-//return bool;
+}
+
 //alert("Sodergimoe stroki: "+str);
  for(var i=0;i<s;i++)
  {
   if(str.charAt(0)==bracketsConfig[i][1])
   {
-    //alert("pervi zakr skobka");
+   // alert("pervi zakr skobka");
    bool=false;
   }
 
@@ -85,7 +135,10 @@ return newstr;
     //alert("raznie skobki");
     bool=false;
   }
+  //alert("vozvrahaem  "+bool);
 return bool;
-}
+
+
+
 
 }
