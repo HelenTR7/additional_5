588e2b95d9dedc7c610372264070817db384c372
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-27 12:11:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bool=true;//значение ответа функции
+  var i=str.length-1;
+  i=+i;  
+  //alert("V stroke simvolov:  "+(i+1));// длина строки
+  if(((i+1)%2)==1)
+  {
+    //alert("Nehetnoe kol-vo, skobki neverni");
+    bool=false;
+  }
+
+  for(var j=i; j>0; j--)//пробегаем по строке
+  {
+var a=true;
+var elem=str.charAt(j);//доступ к элементам строки
+//alert(elem);
+//alert(bracketsConfig[0][0]);//доступ к элементам подмассивов
+//i--;
+if(elem==bracketsConfig[0[0]]){}
+
+
+  }
+return bool;
+
 }
